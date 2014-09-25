cc2070efcda8f8b1d885513cd392f88e854f1784
subeenjeon <powersubeen@naver.com>
2014-09-25 14:09:42 +0900

    d

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
-var trailX = 0;
-var trailY = 0;
+var trailX0 = 0;
+var trailX1 = 0;
+var trailY0 = 0;
+var trailY1 = 0;
 
 function setup() {
 	createCanvas(800, 600);
@@ -9,8 +11,18 @@ function setup() {
 
 function draw() {
 	background(0);
+
+	// mouse ellipse
 	ellipse(mouseX, mouseY, 100, 100);
-	trailX = (mouseX + trailX * 9) / 10;
-	trailY = (mouseY + trailY * 9) / 10;
+
+	// trail ellipse #0
+	trailX0 = (mouseX + trailX0 * 9) / 10;
+	trailY0 = (mouseY + trailY0 * 9) / 10;
 	ellipse(trailX, trailY, 80, 80);
+
+	// trail ellipse #1
+	trailX1 = (trailX0 + trailX1 * 9) / 10;
+	trailY1 = (trailY0 + trailY1 * 9) / 10;
+	ellipse(trailX, trailY, 80, 80);
+
 }
