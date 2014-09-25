8926a2e57dffb0a28e7665042c9fa7ed9118c2ad
subeenjeon <powersubeen@naver.com>
2014-09-25 14:22:52 +0900

    d

--- sketch.js
+++ sketch.js
@@ -2,10 +2,15 @@ var trailX0 = 0;
 var trailX1 = 0;
 var trailX2 = 0;
 var trailX3 = 0;
+
+var trailX = [0, 0, 0, 0];
+
 var trailY0 = 0;
 var trailY1 = 0;
 var trailY2 = 0;
 var trailY3 = 0;
+
+var trailY = [0, 0, 0, 0];
 
 
 
@@ -21,24 +26,30 @@ function draw() {
 	// mouse ellipse
 	ellipse(mouseX, mouseY, 100, 100);
 
+	for (var i = 1; i < 4; i = i +1) {
+		trailX[i] = (trailX[i-1] + trailX[i] * 9) /10;
+		trailY[i] = (trailY[i-1] + trailY[i] * 9) /10;
+		ellipse(trailX[i], trailY[i], 80, 80);
+	}
+/*
 	// trail ellipse #0
-	trailX0 = (mouseX + trailX0 * 9) / 10;
-	trailY0 = (mouseY + trailY0 * 9) / 10;
-	ellipse(trailX0, trailY0, 80, 80);
+	trailX[0] = (mouseX + trailX[0] * 9) / 10;
+	trailY[0] = (mouseY + trailY[0] * 9) / 10;
+	ellipse(trailX[0], trailY[0], 80, 80);
 
 	// trail ellipse #1
-	trailX1 = (trailX0 + trailX1 * 9) / 10;
-	trailY1 = (trailY0 + trailY1 * 9) / 10;
-	ellipse(trailX1, trailY1, 80, 80);
+	trailX[1] = (trailX[0] + trailX[1] * 9) / 10;
+	trailY[1] = (trailY[0] + trailY[1] * 9) / 10;
+	ellipse(trailX[1], trailY[1], 80, 80);
 
 	// trail ellipse #2
-	trailX2 = (trailX1 + trailX2 * 9) / 10;
-	trailY2 = (trailY1 + trailY2 * 9) / 10;
-	ellipse(trailX2, trailY2, 80, 80);
+	trailX[2] = (trailX[1] + trailX[2] * 9) / 10;
+	trailY[2] = (trailY[1] + trailY[2] * 9) / 10;
+	ellipse(trailX[2], trailY[2], 80, 80);
 
 	// trail ellipse #3
-	trailX3 = (trailX2 + trailX3 * 9) / 10;
-	trailY3 = (trailY2 + trailY3 * 9) / 10;
-	ellipse(trailX3, trailY3, 80, 80);
-
+	trailX[3] = (trailX[2] + trailX[3] * 9) / 10;
+	trailY[3] = (trailY[2] + trailY[3] * 9) / 10;
+	ellipse(trailX[3], trailY[3], 80, 80);
+*/
 }
