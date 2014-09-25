f4bf9055886fbcacf21b46e18e37919466a7a6a5
subeenjeon <powersubeen@naver.com>
2014-09-25 14:25:31 +0900

    d

--- sketch.js
+++ sketch.js
@@ -1,17 +1,17 @@
+/*
 var trailX0 = 0;
 var trailX1 = 0;
 var trailX2 = 0;
 var trailX3 = 0;
 
-var trailX = [0, 0, 0, 0];
-
 var trailY0 = 0;
 var trailY1 = 0;
 var trailY2 = 0;
 var trailY3 = 0;
+*/
 
+var trailX = [0, 0, 0, 0];
 var trailY = [0, 0, 0, 0];
-
 
 
 function setup() {
