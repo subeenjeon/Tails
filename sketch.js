5ba8092193d84e2e248d36ffcacb909b3636a2b4
subeenjeon <powersubeen@naver.com>
2014-09-25 14:04:13 +0900

    d

--- sketch.js
+++ sketch.js
@@ -1,3 +1,6 @@
+var trailX = 0;
+var trailY = 0;
+
 function setup() {
 	createCanvas(800, 600);
 	noFill();
@@ -7,4 +10,7 @@ function setup() {
 function draw() {
 	background(0);
 	ellipse(mouseX, mouseY, 100, 100);
+	trailX = (mouseX + trailX * 9) / 10;
+	trailX = (mouseY + trailY * 9) / 10;
+	ellipse(trailX, trailY, 80, 80);
 }
